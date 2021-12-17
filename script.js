ac28642bdd50c3270949688d63b68108368cd6c9
Clodus-NT <clodus.nt@gmail.com>
2021-12-17 14:38:35 -0800

    created pokemonList array / created objects for bulbasaur, charmander, squirtle, pikachu, and gengar / assigned name, height, and types keys to each pokemon

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
-alert('Hello World');
+let pokemonList = [
+    let bulbasaur = {
+        name: 'Bulbasaur',
+        height: '0.7 m',
+        types: [
+            'grass',
+            'poison'
+        ]
+    },
 
-let favFood = 'sushi'
-document.write(favFood);
+    let charmander = {
+        name: 'Charmander',
+        height: '0.6 m',
+        types = [
+            'fire'
+        ]
+    },
+
+    let squirtle = {
+        name: 'Squirtle',
+        height: '0.5 m',
+        types = [
+            'water'
+        ]
+    },
+
+    let pikachu = {
+        name: 'Pikachu',
+        height: '0.4 m',
+        types = [
+            'electric'
+        ],
+
+    let gengar = {
+        name: 'Gengar',
+        height: '1.5 m',
+        types = [
+            'ghost',
+            'poison'
+        ]
+    }
+    }
+]
