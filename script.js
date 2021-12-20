850f2b709f162d99b255d5e02196246542a97279
Clodus-NT <clodus.nt@gmail.com>
2021-12-20 14:56:27 -0800

    created for loop that checks the height of each pokemon and prints a corresponding message to the page

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+//Array that contains pokemon
+//and their respective stats
 let pokemonList = [
     {
         name: 'Bulbasaur',
-        height: '0.7 m',
+        height: '0.7',
         types: [
             'grass',
             'poison'
@@ -10,7 +12,7 @@ let pokemonList = [
 
     {
         name: 'Charmander',
-        height: '0.6 m',
+        height: 0.6,
         types: [
             'fire'
         ]
@@ -18,7 +20,7 @@ let pokemonList = [
 
     {
         name: 'Squirtle',
-        height: '0.5 m',
+        height: 0.5,
         types: [
             'water'
         ]
@@ -26,15 +28,15 @@ let pokemonList = [
 
     {
         name: 'Pikachu',
-        height: '0.4 m',
+        height: 0.4,
         types: [
             'electric'
-        ],
+        ]
     },
 
     {
         name: 'Gengar',
-        height: '1.5 m',
+        height: 1.5,
         types: [
             'ghost',
             'poison'
@@ -42,3 +44,16 @@ let pokemonList = [
     }
     
 ]
+
+//Checks the height of each pokemon
+//and prints corresponding message
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1) {
+        document.write(
+            pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + " - Wow! That's a big Pokemon" + "<br>");
+    } else {
+        document.write(
+            pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br>");
+    }
+}
+
