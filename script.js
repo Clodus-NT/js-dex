dd67b340493c40c758e48ea96dbe3b4c3fbfa271
Clodus-NT <clodus.nt@gmail.com>
2022-01-13 09:49:01 -0800

    wrapped pokemonList array in IIFE / in process of rewriting for loop to use .forEach() method

--- script.js
+++ script.js
@@ -1,59 +1,85 @@
-//Array that contains pokemon
-//and their respective stats
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: '0.7',
-        types: [
-            'grass',
-            'poison'
-        ]
-    },
-
-    {
-        name: 'Charmander',
-        height: 0.6,
-        types: [
-            'fire'
-        ]
-    },
-
-    {
-        name: 'Squirtle',
-        height: 0.5,
-        types: [
-            'water'
-        ]
-    },
-
-    {
-        name: 'Pikachu',
-        height: 0.4,
-        types: [
-            'electric'
-        ]
-    },
-
-    {
-        name: 'Gengar',
-        height: 1.5,
-        types: [
-            'ghost',
-            'poison'
-        ]
-    }
+//IIFE containing stats array
+let pokemonRepository = (function() {
+    //Array that contains pokemon
+    //and their respective stats
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            types: [
+                'grass',
+                'poison'
+            ]
+        },
     
-]
+        {
+            name: 'Charmander',
+            height: 0.6,
+            types: [
+                'fire'
+            ]
+        },
+    
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            types: [
+                'water'
+            ]
+        },
+    
+        {
+            name: 'Pikachu',
+            height: 0.4,
+            types: [
+                'electric'
+            ]
+        },
+    
+        {
+            name: 'Gengar',
+            height: 1.5,
+            types: [
+                'ghost',
+                'poison'
+            ]
+        }
+        
+    ]
+
+    function add(item) {
+        pokemonList.push(item);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+
+})();
+
 
 //Checks the height of each pokemon
 //and prints corresponding message
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1) {
-        document.write(
-            pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + " - Wow! That's a big Pokemon" + "<br>");
-    } else {
-        document.write(
-            pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br>");
-    }
-}
 
+// for (let i = 0; i < pokemonList.length; i++) {
+//     if (pokemonList[i].height > 1) {
+//         document.write(
+//             pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + " - Wow! That's a big Pokemon" + "<br>");
+//     } else {
+//         document.write(
+//             pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br>");
+//     }
+// }
+
+pokemonRepository.forEach(function(pokemon) {
+    document.write(
+        // pokemonList.name + '<br>' + 'Height: ' + pokemonList.height + '<br>' + 'Type: ' + pokemonList.types + '<br>' + '<br>'
+        pokemonRepository.getAll()
+    );
+    }
+)
